b8daf76584a23bf655f5435bea7e6a6892522c83
Kolesnikov <kolesnikov.de@mail.ru>
2018-11-15 21:05:20 +0400

    work page complete

---
+++ blocks/image-slider/_control/image-slider_control_tabs.deps.js
@@ -0,0 +1,15 @@
+({  
+  mustDeps: [{
+    block: 'image-slider',
+    elem: 'tab'
+  }, 
+  {
+    block: 'image-slider',
+    elem: 'tab',
+    mod: 'active'
+  }, 
+  {
+    block: 'image-slider',
+    elem: 'tabs'
+  }]
+})
