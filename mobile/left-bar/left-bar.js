44b1131d2105d5b0a3921056038f7993ab0feabe
Kolesnikov <kolesnikov.de@mail.ru>
2018-12-15 11:20:20 +0400

    Review changes; some cleanup

--- mobile/left-bar/left-bar.js
+++ mobile/left-bar/left-bar.js
@@ -1,8 +1,13 @@
 $(document).on('click', function(e){
   var target = $(e.target);
   var bar = $('.left-bar');
-  if (target.parent('.left-bar__label').length){
+  
+  if (target.hasClass('left-bar__toggle')){
     bar.addClass('left-bar_active');
+    $('.logo-m').addClass('logo-m_hidden');
   }
-  else if (!target.parents('.left-bar').length && !target.hasClass('left-bar') && bar.hasClass('left-bar_active')) bar.removeClass('left-bar_active');
+  else if (!target.parents('.left-bar').length && !target.hasClass('left-bar') && bar.hasClass('left-bar_active')){
+    bar.removeClass('left-bar_active');
+    $('.logo-m').removeClass('logo-m_hidden');
+  }
 });
